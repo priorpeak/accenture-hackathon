53ee17fbc465a4cc09107b97d0f280d96c14aa6d
Madeline Wong <Madeline@Madelines-MBP-4.home>
2020-07-17 16:15:27 -0400

    dataset created

--- data.py
+++ data.py
@@ -2,57 +2,58 @@ import random
 from datetime import datetime
 from openpyxl import load_workbook
 
+def get_value(cell):
+	return cell.value
+
 random.seed(datetime.now())
 
-n_prof = 10
-n_proj = 50
+n_cand = 1000
+n_role = 5000
 
-vars_fn = "filename_here"
 prof_fn = "profile_data.csv"
 proj_fn = "project_data.csv"
 
-wb = load_workbook(filename='C:\Users\alexa\Desktop\Developer\accenture-hackathon\Dataset.xlsx')
+wb = load_workbook(filename='Dataset.xlsx')
 ws = wb['Sheet1']
-'''
-TODO: load spreadsheet into Python
-'''
 
 candidate_vars = dict()
 role_vars = dict()
 
 for col in ws.iter_cols(min_col=1, max_col=9, min_row=2):
-    candidate_vars[col[0]] = col[1:]
+	candidate_vars[col[0].value] = [i for i in map(get_value, col[1:]) if i is not None]
+
+candidate_keys = candidate_vars.keys()
 
 for col in ws.iter_cols(min_col=11, max_col=18, min_row=2):
-    role_vars[col[0]] = col[1:]
-'''
-TODO: make data structure with all profile variables and values
-ex. {'Service': ['Technology', 'Strategy & Consulting', 'Interactive', 'Operations'],
-	 'Talent Segment': ['Account Leadership, Business', ...],
-	 ...}
-'''
+	role_vars[col[0].value] = [i for i in map(get_value, col[1:]) if i is not None]
+
+role_keys = role_vars.keys()
+
 with open(prof_fn, "w+") as profiles:
-    for uid in range(n_prof):
-        line = ""
-        '''
-		TODO: randomly select profile values from each profile variable
-		and append to line separated by comma
-		'''
-        profiles.write(line + "\n")
+	line = "UID,"
+	for key in candidate_keys:
+		line += key + ","
+	line = line[:-1]
+	profiles.write(line)
+
+	for uid in range(n_cand):
+		line = str(uid) + ","
+		for key in candidate_keys:
+			line += str(random.choice(candidate_vars[key])) + ","
+
+		profiles.write("\n" + line[:-1])
 
 
-project_vars = dict()
-'''
-TODO: make data structure with all project variables and values
-ex. {'Chargeability': ['Yes', 'No'],
-	 'Start Date': ['1/1/2020', '2/1/2020', ...],
-	 ...}
-'''
 with open(proj_fn, "w+") as projects:
-    for uid in range(n_proj):
-        line = ""
-        '''
-		TODO: randomly select project values from each project variable
-		and append to line separated by comma
-		'''
-        projects.write(line + "\n")
+	line = "UID,"
+	for key in role_keys:
+		line += key + ","
+	line = line[:-1]
+	projects.write(line)
+
+	for uid in range(n_role):
+		line = str(uid) + ","
+		for key in role_keys:
+			line += str(random.choice(role_vars[key])) + ","
+
+		projects.write("\n" + line[:-1])
