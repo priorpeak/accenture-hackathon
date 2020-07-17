ba72bbade55f223cdba46524114c8e84f125e2d3
Madeline Wong <Madeline@Madelines-MBP-4.home>
2020-07-17 10:40:47 -0400

    initial framework for data generation script

---
+++ data.py
@@ -0,0 +1,44 @@
+import random
+
+random.seed(20200717)
+
+n_prof = 10
+n_proj = 50
+
+vars_fn = "filename_here"
+prof_fn = "profile_data.csv"
+proj_fn = "project_data.csv"
+
+with open(vars_fn, "r") as vf:
+	profile_vars = dict()
+	'''
+	TODO: make data structure with all profile variables and values
+	ex. {'Service': ['Technology', 'Strategy & Consulting', 'Interactive', 'Operations'],
+		 'Talent Segment': ['Account Leadership, Business', ...],
+		 ...}
+	'''
+	with open(prof_fn, "w+") as profiles:
+		for uid in range(n_prof):
+			line = ""
+			'''
+			TODO: randomly select profile values from each profile variable
+			and append to line separated by comma
+			'''
+			profiles.write(line + "\n")
+
+
+	project_vars = dict()
+	'''
+	TODO: make data structure with all project variables and values
+	ex. {'Chargeability': ['Yes', 'No'],
+		 'Start Date': ['1/1/2020', '2/1/2020', ...],
+		 ...}
+	'''
+	with open(proj_fn, "w+") as projects:
+		for uid in range(n_proj):
+			line = ""
+			'''
+			TODO: randomly select project values from each project variable
+			and append to line separated by comma
+			'''
+			projects.write(line + "\n")
