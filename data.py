c94a4bef9b5e3aa8ec905b989026d826fcb77441
Madeline Wong <Madeline@Madelines-MBP-4.home>
2020-07-17 16:58:52 -0400

    fixed role data dates

--- data.py
+++ data.py
@@ -10,8 +10,8 @@ random.seed(datetime.now())
 n_cand = 1000
 n_role = 5000
 
-prof_fn = "candidate_data.csv"
-proj_fn = "project_data.csv"
+prof_fn = 'candidate_data.csv'
+proj_fn = 'project_data.csv'
 
 wb = load_workbook(filename='Dataset.xlsx')
 ws = wb['Sheet1']
@@ -29,31 +29,47 @@ for col in ws.iter_cols(min_col=11, max_col=18, min_row=2):
 
 role_keys = role_vars.keys()
 
-with open(prof_fn, "w+") as profiles:
-	line = "UID,"
+with open(prof_fn, 'w+') as profiles:
+	line = 'UID,'
 	for key in candidate_keys:
-		line += key + ","
+		line += key + ','
 	line = line[:-1]
 	profiles.write(line)
 
 	for uid in range(n_cand):
-		line = str(uid) + ","
+		line = str(uid) + ','
 		for key in candidate_keys:
-			line += str(random.choice(candidate_vars[key])) + ","
+			line += str(random.choice(candidate_vars[key])) + ','
 
-		profiles.write("\n" + line[:-1])
+		profiles.write('\n' + line[:-1])
 
 
-with open(proj_fn, "w+") as projects:
-	line = "UID,"
+with open(proj_fn, 'w+') as projects:
+	line = 'UID,'
 	for key in role_keys:
-		line += key + ","
+		line += key + ','
 	line = line[:-1]
 	projects.write(line)
 
 	for uid in range(n_role):
-		line = str(uid) + ","
-		for key in role_keys:
-			line += str(random.choice(role_vars[key])) + ","
+		line = str(uid) + ','
 
-		projects.write("\n" + line[:-1])
+		start_date_idx = None
+		start_date = None
+		end_date = None
+
+		for key in role_keys:
+			elem = random.choice(role_vars[key])
+			if key == 'Start Date':
+				start_date_idx = role_vars[key].index(elem)
+				start_date = elem
+			elif key == 'End Date':
+				elem = random.choice(role_vars[key][start_date_idx:])
+				end_date = elem
+			
+			line += str(elem) + ','
+
+		assert start_date < end_date
+		projects.write('\n' + line[:-1])
+
+
