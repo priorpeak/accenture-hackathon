4af495716cce2b0f64145f1a42a9ae7b6151d64e
priorpeak <alexanderjprior@gmail.com>
2020-07-17 15:59:39 -0400

    Initialized dictionaries

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import random
 from datetime import datetime
+from openpyxl import load_workbook
 
 random.seed(datetime.now())
 
@@ -10,11 +11,20 @@ vars_fn = "filename_here"
 prof_fn = "profile_data.csv"
 proj_fn = "project_data.csv"
 
+wb = load_workbook(filename='C:\Users\alexa\Desktop\Developer\accenture-hackathon\Dataset.xlsx')
+ws = wb['Sheet1']
 '''
 TODO: load spreadsheet into Python
 '''
 
-profile_vars = dict()
+candidate_vars = dict()
+role_vars = dict()
+
+for col in ws.iter_cols(min_col=1, max_col=9, min_row=2):
+    candidate_vars[col[0]] = col[1:]
+
+for col in ws.iter_cols(min_col=11, max_col=18, min_row=2):
+    role_vars[col[0]] = col[1:]
 '''
 TODO: make data structure with all profile variables and values
 ex. {'Service': ['Technology', 'Strategy & Consulting', 'Interactive', 'Operations'],
@@ -22,13 +32,13 @@ ex. {'Service': ['Technology', 'Strategy & Consulting', 'Interactive', 'Operatio
 	 ...}
 '''
 with open(prof_fn, "w+") as profiles:
-	for uid in range(n_prof):
-		line = ""
-		'''
+    for uid in range(n_prof):
+        line = ""
+        '''
 		TODO: randomly select profile values from each profile variable
 		and append to line separated by comma
 		'''
-		profiles.write(line + "\n")
+        profiles.write(line + "\n")
 
 
 project_vars = dict()
@@ -39,10 +49,10 @@ ex. {'Chargeability': ['Yes', 'No'],
 	 ...}
 '''
 with open(proj_fn, "w+") as projects:
-	for uid in range(n_proj):
-		line = ""
-		'''
+    for uid in range(n_proj):
+        line = ""
+        '''
 		TODO: randomly select project values from each project variable
 		and append to line separated by comma
 		'''
-		projects.write(line + "\n")
+        projects.write(line + "\n")
