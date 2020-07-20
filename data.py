41f69a9eff5f4c75a46a5114d586f278937fad8a
Prior <alexander.j.prior@accenture.com>
2020-07-20 11:55:38 -0400

    Added comments

--- data.py
+++ data.py
@@ -1,48 +1,30 @@
 import random
 from datetime import datetime
+from openpyxl import load_workbook
 
 random.seed(datetime.now())
 
+# Set number of profiles and projects to generate for randomized dataset
 n_prof = 10
 n_proj = 50
 
+# Input filenames to pull random data from
 vars_fn = "filename_here"
 prof_fn = "profile_data.csv"
 proj_fn = "project_data.csv"
 
-'''
-TODO: load spreadsheet into Python
-'''
+# Load sample data workbook
+wb = load_workbook(filename = 'C:\Users\alexander.j.prior\OneDrive - Accenture\Desktop\Hackathon\accenture-hackathon\dataset.xlsx')
+ws = wb['Sheet1']
 
-profile_vars = dict()
-'''
-TODO: make data structure with all profile variables and values
-ex. {'Service': ['Technology', 'Strategy & Consulting', 'Interactive', 'Operations'],
-	 'Talent Segment': ['Account Leadership, Business', ...],
-	 ...}
-'''
+# Randomly populate profile attributes from profile_data.csv
 with open(prof_fn, "w+") as profiles:
 	for uid in range(n_prof):
 		line = ""
-		'''
-		TODO: randomly select profile values from each profile variable
-		and append to line separated by comma
-		'''
 		profiles.write(line + "\n")
 
-
-project_vars = dict()
-'''
-TODO: make data structure with all project variables and values
-ex. {'Chargeability': ['Yes', 'No'],
-	 'Start Date': ['1/1/2020', '2/1/2020', ...],
-	 ...}
-'''
+# Randomly populate project attributes from project_data.csv
 with open(proj_fn, "w+") as projects:
 	for uid in range(n_proj):
 		line = ""
-		'''
-		TODO: randomly select project values from each project variable
-		and append to line separated by comma
-		'''
 		projects.write(line + "\n")
